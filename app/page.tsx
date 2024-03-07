62076aebe7f6f618ed6194b36f9fc4a5c5d65bb9
rahmatadlin <rahmatadlin210@gmail.com>
2024-03-07 22:35:19 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,52 @@
-export default function Home() {
-  return <h1 className="title text-2xl">Home Page</h1>;
-}
+// import Link from 'next/link';
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1 className="text-2xl font-bold">Home Page</h1>
+//       <Link href="/contacts">
+//         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block mt-4">Go to Contacts</button>
+//       </Link>
+//     </div>
+//   );
+// }
+
+
+import ContactTable from "@/components/contact-table";
+import Search from "@/components/search";
+import Pagination from "@/components/pagination";
+import { CreateButton } from "@/components/buttons";
+import { getContactPages } from "@/lib/data";
+import { Suspense } from "react";
+import { TableSkeleton } from "@/components/skeleton";
+
+const Contacts = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await getContactPages(query);
+
+  return (
+    <div className="max-w-screen-md mx-auto mt-5">
+      <div className="flex items-center justify-between gap-1 mb-5">
+        <Search />
+        <CreateButton />
+      </div>
+      <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
+        <ContactTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="flex justify-center mt-4">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
